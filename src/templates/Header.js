98b12476d735c94e46887159d085021063fda4e6
oswaldoPedroza7 <91993660+oswaldoPedroza7@users.noreply.github.com>
2022-03-27 22:49:23 -0500

    Add files via upload

---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    const view = `
+      <div class="Header-main">
+      <div class="Header-logo">
+            <a href="#">
+               <img src="src/images/rm-logo.png"">
+            </a>
+      </div>
+        <div class="Header-nav">
+          <a href="#/about/">
+            About
+          </a>
+        </div>
+      </div>
+    `;
+    return view;
+  };
+  
+  export default Header;
